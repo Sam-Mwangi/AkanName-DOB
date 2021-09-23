2a5c000e44f73e0b197887c3d8698b35e47f5051
jebitok-dev <sharonjebitok@gmail.com>
2021-09-23 11:53:46 +0300

    Add Initial Files

---
+++ index.js
@@ -0,0 +1,21 @@
+var akanGender = [
+    {
+        "Sunday": "Kwasi",
+        "Monday": "Kwadwo",
+        "Tuesday": "Kwabena",
+        "Wednesday": "Kwaku",
+        "Thurday": "Yaw",
+        "Friday": "Kofi",
+        "Saturday": "Kwame"
+    },
+    {
+        "Sunday": "Akosua",
+        "Monday": "Adwoa",
+        "Tuesday": "Abenaa",
+        "Wednesday": "Akua",
+        "Thurday": "Yaa",
+        "Friday": "Afua",
+        "Saturday": "Ama"
+
+    }
+]
