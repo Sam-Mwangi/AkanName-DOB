51ba35474e55e33661f6664d9384aeabcd316d83
jebitok-dev <sharonjebitok@gmail.com>
2021-09-23 11:57:51 +0300

    Add Changes to JS File

--- index.js
+++ index.js
@@ -23,12 +23,36 @@ var akanGender = [
 var weekDayArray = ["Sunday", "Monday", "Tuesday", "Wednesday", "Thurday", "Friday", "Saturday"];
 var colorIndex = 0;
 
-function changeBackgroundColor() {
-    var doc = document.getElementsByTagName("BODY")[0];
-    var color = ["#49599a", "#883997", "#0069c0", "#75a478", "#00867d", "#087f23"];
-    doc.style.backgroundColor = color[colorIndex];
-    colorIndex = (colorIndex + 1) % color.length;
+function validateForm() {
+    var gender = document.getElementsByName("gender");
+    var mdate = document.getElementById("mdate");
+    var mmonth = document.getElementById("mmonth");
+    var myear = document.getElementById("myear");
+    var formValid = false;
+    var i = 0;
+    document.getElementById("result").innerHTML = "";
+    if (mdate.value == "" || mdate.value == null) {
 
-}
-setInterval(changeBackgroundColor, 2000);
+        document.getElementById("result").innerHTML += "Day is Required";
+        document.getElementById("result").style.color = 'red';
 
+        mdate.style.border = "2px solid red";
+        return false;
+    } else {
+        mdate.style.border = "";
+        if (!isNaN(mdate.value)) {
+            if (mdate.value <= 0 || mdate.value > 31) {
+                document.getElementById("result").innerHTML += "Invalid Day";
+                document.getElementById("result").style.color = 'red';
+
+                mdate.style.border = "2px solid red";
+                return false;
+            }
+        } else {
+            document.getElementById("result").innerHTML += "Day must be a number";
+            document.getElementById("result").style.color = 'red';
+
+            mdate.style.border = "2px solid red";
+            return false;
+        }
+    }
